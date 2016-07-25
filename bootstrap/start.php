f2173c96a988cafb98a32ffce2939f7c833df993
jsteedle <jesse.steedle@gmail.com>
2016-07-24 21:07:41 -0500

    start -> production -> IP address

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,9 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	
+	'production' => array('104.236.232.80'),
 	'local' => array('homestead'),
-	'production' => array('forge')
+	
 	
 
 ));
