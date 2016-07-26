819f7f0ee96904ff361796b2c8be9acd0c1100c8
jsteedle <jesse.steedle@gmail.com>
2016-07-25 19:53:09 -0500

    start

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'production' => array('104.236.232.80'),
+	//'production' => array('104.236.232.80'),
 	'local' => array('homestead')
 	
 	
